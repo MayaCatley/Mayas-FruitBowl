67a1e5b55160378a701d93b92930e4aa3115a1c2
MayaCatley <maya.catley@marsden.school.nz>
2021-09-12 15:15:05 +1200

    Update

--- testing.py
+++ testing.py
@@ -1,36 +1,22 @@
-from datetime import date
-import re
+def get_string(m):
+    user_input = input(m).strip()
+    return user_input
 
-def validate_phone(str):
-    str = re.sub(r"\D", "", str)
-    valid = True
 
-    if re.match(r"^(64|04|02)", str):
-        if re.match(r"^64", str) and (len(str) < 10 or len(str) > 13):
-            valid = False
-        elif re.match(r"^04", str) and (len(str) < 8 or len(str) > 9):
-            valid = False
-        elif re.match(r"^(021|027)", str) and (len(str) < 10 or len(str) > 11):
-            valid = False
+def validate_name(name):
+    if len(name) < 2 or len(name) > 30:
+        print("The name must be between 2 and 30 characters.")
+        return pick_name()
     else:
-        valid = False
+        return name
 
-    print(str, valid)
 
-def main():
-    validate_phone("0000")
-    validate_phone("04-555-555")
-    validate_phone("04-555-5555")
-    validate_phone("04 555 5555")
-    validate_phone("64 04 555 5555")
-    validate_phone("64 21 555 5555")
-    validate_phone("021 555 5555")
-    validate_phone("021 5555")
-    validate_phone("64 021 5555")
-    validate_phone("64 021 5555 444")
+def pick_name():
+    name = get_string("What is your name: -> ")
+    return validate_name(name)
 
-if __name__ == "__main__":
-    main()
+pick_name()
+
 
 
 
